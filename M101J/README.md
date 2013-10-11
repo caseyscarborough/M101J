b22cb8ff3935b0cf4e992400fca0d4567acaefc6
Casey Scarborough <caseyscarborough@gmail.com>
2013-10-11 19:09:39 -0400

    Add README.md and .gitignore to M101J project. Remove unnecessary spacing from HelloWorldFreemarkerStyle.

---
+++ M101J/README.md
@@ -0,0 +1,3 @@
+# M101J Project Files
+
+This is the code for the M101J course.
--- M101J/src/main/java/com/caseyscarborough/HelloWorldFreemarkerStyle.java
+++ M101J/src/main/java/com/caseyscarborough/HelloWorldFreemarkerStyle.java
@@ -1,34 +1,34 @@
-    package com.caseyscarborough;
+package com.caseyscarborough;
 
-    import freemarker.template.Configuration;
-    import freemarker.template.Template;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 
-    import java.io.StringWriter;
-    import java.util.HashMap;
-    import java.util.Map;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-    public class HelloWorldFreemarkerStyle {
-        public static void main(String[] args) {
-            // Set the configuration for where to find the template
-            Configuration configuration = new Configuration();
-            configuration.setClassForTemplateLoading(HelloWorldFreemarkerStyle.class, "/");
+public class HelloWorldFreemarkerStyle {
+    public static void main(String[] args) {
+        // Set the configuration for where to find the template
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldFreemarkerStyle.class, "/");
 
-            try {
-                // Create a new template and string writer
-                Template helloTemplate = configuration.getTemplate("hello.ftl");
-                StringWriter writer = new StringWriter();
+        try {
+            // Create a new template and string writer
+            Template helloTemplate = configuration.getTemplate("hello.ftl");
+            StringWriter writer = new StringWriter();
 
-                // Create a map to put the data bindings into
-                Map<String, Object> helloMap = new HashMap<String, Object>();
-                helloMap.put("name", "Freemarker");
+            // Create a map to put the data bindings into
+            Map<String, Object> helloMap = new HashMap<String, Object>();
+            helloMap.put("name", "Freemarker");
 
-                // Process the template with the map and put the data into writer
-                helloTemplate.process(helloMap, writer);
+            // Process the template with the map and put the data into writer
+            helloTemplate.process(helloMap, writer);
 
-                // Print out the results
-                System.out.println(writer);
-            } catch (Exception e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
+            // Print out the results
+            System.out.println(writer);
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
     }
+}
